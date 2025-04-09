adaaf052d70060626a9c0b712843621d1a3f7a29
shafin-10 <shafinshahriar28@gmail.com>
2025-04-09 13:42:27 +0600

    added lombok library

--- src/main/java/com/example/EazySchool/Model/Holiday.java
+++ src/main/java/com/example/EazySchool/Model/Holiday.java
@@ -1,5 +1,8 @@
 package com.example.EazySchool.Model;
 
+import lombok.Data;
+
+@Data
 public class Holiday {
 
     private final String day;
@@ -10,30 +13,5 @@ public class Holiday {
         FESTIVAL, FEDERAL
     }
 
-    public Holiday(String day, String reason, Type type) {
-        this.day = day;
-        this.reason = reason;
-        this.type = type;
-    }
 
-    public String getDay() {
-        return day;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    @Override
-    public String toString() {
-        return "Holiday{" +
-                "day='" + day + '\'' +
-                ", reason='" + reason + '\'' +
-                ", type=" + type +
-                '}';
-    }
 }
--- src/main/java/com/example/EazySchool/Service/ContactService.java
+++ src/main/java/com/example/EazySchool/Service/ContactService.java
@@ -1,13 +1,13 @@
 package com.example.EazySchool.Service;
 
 import com.example.EazySchool.Model.Contact;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
+@Slf4j
 @Service
 public class ContactService {
-    Logger log = LoggerFactory.getLogger(ContactService.class);
 
     public boolean saveMessageDetails(Contact contact){
         boolean isSaved = true;
